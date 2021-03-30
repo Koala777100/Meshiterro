e3f1d07c9dca309707f20599e446a649870a4089
Koala777100 <s_n_s_h@outlook.jp>
2021-03-30 11:53:24 +0000

    [3-4]Start

--- meshiterro/config/initializers/application_controller_renderer.rb
+++ meshiterro/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '82d662e3388c7a8bb2ba3510b7df3469627e141afaed53c65cca90822afe395f76c46ac6b22cc135361457e773624fffc7be66f436717d8ddd5f4b77e55a8ce9'
